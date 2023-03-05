d95627bf7a6cfd65f274792d3a481859dce0637e
makis tsakalis <maktsakalis@gmail.com>
2023-03-05 22:38:42 +0200

    Addition of tests

---
+++ SpringStartHere/login-ch9/src/test/java/com/springstarthere/loginch9/controllers/LoginControllerTest.java
@@ -0,0 +1,36 @@
+package com.springstarthere.loginch9.controllers;
+
+import com.springstarthere.loginch9.model.LoginProcessor;
+import com.springstarthere.loginch9.model.LoginRequest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.ui.Model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class LoginControllerTest {
+
+    @Mock
+    private Model model;
+
+    @Mock
+    private LoginProcessor loginProcessor;
+
+    @InjectMocks
+    private LoginController loginController;
+
+    @Test
+    void loginPostSuccessTest() {
+
+        given(loginProcessor.login()).willReturn(true);
+        String result = loginController.loginPost(new LoginRequest("Makis", "123"), model);
+
+        assertEquals("redirect:/main", result);
+    }
+}
